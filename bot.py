0a9d7e1e951ac703a445d16fe3b9d59b82feefaa
pro-extremecoder <vladislavmustyatsa@gmail.com>
2020-11-02 21:06:40 +0200

    After adding callback routing

--- bot.py
+++ bot.py
@@ -14,18 +14,36 @@ def start(message):
 	bot.send_message(chat_id=message.chat.id, aim="to_greet", text="Welcome")
 
 	bot.send_message(chat_id=message.chat.id, aim="get_mood", text="How are you?")
+	markup = types.InlineKeyboardMarkup(row_width=2)
+	item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+	item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+	markup.add(item1, item2)
+
+	bot.send_message(chat_id=message.chat.id, aim="quiz", text='.', reply_markup=markup)
 
 
 @bot.message_route('get_mood')
 def get_mood(message):
 	bot.send_message(chat_id=message.chat.id, text="Great")
-	bot.send_message(chat_id=message.chat.id, aim="get_family_mood", text="How is your family?")
+	markup = types.InlineKeyboardMarkup(row_width=2)
+	item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+	item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
 
-@bot.message_route('get_family_mood')	
-def get_family_mood(message):
-	bot.send_message(chat_id=message.chat.id, text="Cool")
+	markup.add(item1, item2)
+	bot.send_message(chat_id=message.chat.id, aim="get_family_mood", text="How is your family?", reply_markup=markup)
+
+@bot.callback_route('get_family_mood')	
+def get_family_mood(call):
+	bot.send_message(chat_id=call.message.chat.id, text="WAW")
+
+@bot.callback_route('quiz')
+def quiz(call):
+	if call.data == 'good':
+		bot.send_message(chat_id=call.message.chat.id, text="Great")
+	else:
+		bot.send_message(chat_id=call.message.chat.id, text="Miserable")
 
 
 
 bot.polling(none_stop=True)
-
--- infrastructure.py
+++ infrastructure.py
@@ -31,6 +31,8 @@ class ExtendedMessage(types.Message):
 class CustomTeleBot(TeleBot):
 	message_list = MessageList()
 	_message_router = {}
+	_callback_router = {}
+	_involved_functions = [] 
 
 	def polling(self, *args, **kwargs):
 		@self.message_handler(content_types=['text'])
@@ -57,31 +59,62 @@ class CustomTeleBot(TeleBot):
 						self.send_message(chat_id=message.chat.id, aim="report_about_uncorrect_using", text="You can't answer this message")
 				else:
 					self.send_message(chat_id=message.chat.id, aim="report_about_uncorrect_using", text="You have already answered this message")
-		
+
+		@self.callback_query_handler(func=lambda call: True)
+		def callback_handler(call):
+			if call.message.chat.type == "private":
+				# transform call.message from Message type to ExtendedMessage type
+				answers = list(filter(lambda ans: ans.message_id == call.message.message_id, self.message_list.messages))
+				if answers:
+					call.message = answers[0]
+				else:
+					self.send_message(chat_id=call.message.chat.id, aim="report_about_uncorrect_using", text="Message you've answered is out of current session")
+					return None
+
+				
+				self._callback_router[call.message.aim](call)
+				self.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text,
+                reply_markup=None)
+				call.message.is_answered = True
+				logger.debug(self.message_list)
+				
+
 		super().polling(*args, **kwargs)
 
 	def send_message(self, aim=None, *args, **kwargs):
+
 		message = super().send_message(*args, **kwargs)
+
+		if 'reply_markup' in message.json:
+			if not aim:
+				raise RuntimeError('if you want to send message with reply markup, you have to enter "aim"')
+			if aim not in self._callback_router:
+				raise RuntimeError("This aim doesn't have any handler")
+
 		ex_message = ExtendedMessage(aim=aim, parent_message=message)
 		self.message_list.messages.append(ex_message)
 		return ex_message
 
 	def message_route(self, current_aim):
+		
 		def wrapper(func):
 
-			if current_aim in self._message_router:
+			if current_aim in self._message_router or current_aim in self._callback_router:
 				raise RuntimeError('route for this message aim have already existed')
 
-			for aim in self._message_router:
+			'''for aim in self._message_router:
 				if self._message_router[aim].__name__ == func.__name__:
-					raise RuntimeError(f'function "{func.__name__}" is used for another message aim')
+					raise RuntimeError(f'function "{func.__name__}" is used for another message aim')'''
+			if func.__name__ in self._involved_functions:
+				raise RuntimeError(f'function "{func.__name__}" is used for another message aim')
 			else:
-				logger.info(f"<aim : {current_aim}, function: {func.__name__}>")
+				logger.info(f"<[MESSAGE_HANDLER]aim : {current_aim}, function: {func.__name__}>")
 				self._message_router[current_aim] = func
+				self._involved_functions.append(func.__name__)
 
 			return func
-		return wrapper 
 
+		return wrapper 
 
 	def only_replies(self, func):
 		
@@ -94,6 +127,26 @@ class CustomTeleBot(TeleBot):
 				self.send_message(chat_id=message.chat.id, aim="unapropriate answer", text="You have to only reply")
 
 		return wrapper
+
+	def callback_route(self, current_aim):
+
+		def wrapper(func):
+
+			if current_aim in self._callback_router or current_aim in self._message_router:
+				raise RuntimeError('route for this message aim have already existed')
+
+			if func.__name__ in self._involved_functions:
+				raise RuntimeError(f'function "{func.__name__}" is used for another message aim')
+			else:
+				logger.info(f"<[CALLBACK_HANDLER]aim : {current_aim}, function: {func.__name__}>")
+				self._callback_router[current_aim] = func
+				self._involved_functions.append(func.__name__)
+
+			return func
+
+		return wrapper 
+
+
 
 
 
